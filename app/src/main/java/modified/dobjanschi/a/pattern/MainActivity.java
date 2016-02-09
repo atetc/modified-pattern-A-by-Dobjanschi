4847deb14ad880b0e0e12840e823e3988d51c681
Rustem Saitkulov <srmsam@yandex.ru>
2016-02-09 12:37:53 +0300

    First commit

---
+++ app/src/main/java/modified/dobjanschi/a/pattern/MainActivity.java
@@ -0,0 +1,13 @@
+package modified.dobjanschi.a.pattern;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
